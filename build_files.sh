3f1f7ab0496ed06938a96f03829a669f9e37c32e
Jnwoke <jnwoke@appzonegroup.com>
2024-09-16 16:40:32 +0100

    update new

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Install PostgreSQL development headers
+apt-get update && apt-get install -y libpq-dev
+
+# Install required packages
 python3.12 -m pip install -r requirements.txt
+
+# Run Django collectstatic
 python3.12 manage.py collectstatic --noinput
